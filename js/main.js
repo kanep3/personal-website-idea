62bc4806a48a96403baa37b46152bf4eeb19776f
karl <72425445+kanep3@users.noreply.github.com>
2022-06-09 16:24:26 +0300

    Revert "v2"
    This reverts commit 6b28e89a61614826338f8417e048a201ed3f130c.

---
+++ js/main.js
@@ -0,0 +1,212 @@
+// Slide on scroll
+$(document).ready(function() {
+    var containers = $('.container');
+
+    if (containers.length) {
+        containers.each(function() {
+            var container = $(this);
+
+            // Support small text - copy to fill screen width
+            if (container.find('.scrolling-text').outerWidth() < $(window).width()) {
+                var windowToScrolltextRatio = Math.round($(window).width() / container.find('.scrolling-text').outerWidth()),
+                    scrollTextContent = container.find('.scrolling-text .scrolling-text-content').text(),
+                    newScrollText = '';
+                for (var i = 0; i < windowToScrolltextRatio; i++) {
+                    newScrollText += ' ' + scrollTextContent;
+                }
+                container.find('.scrolling-text .scrolling-text-content').text(newScrollText);
+            }
+
+            // Init variables and config
+            var scrollingText = container.find('.scrolling-text'),
+                scrollingTextWidth = scrollingText.outerWidth(),
+                scrollingTextHeight = scrollingText.outerHeight(true),
+                startLetterIndent = parseInt(scrollingText.find('.scrolling-text-content').css('font-size'), 10) / 4.8,
+                startLetterIndent = Math.round(startLetterIndent),
+                scrollAmountBoundary = Math.abs($(window).width() - scrollingTextWidth),
+                transformAmount = 0,
+                leftBound = 0,
+                rightBound = scrollAmountBoundary,
+                transformDirection = container.hasClass('left-to-right') ? -1 : 1,
+                transformSpeed = 200;
+
+            // Read transform speed
+            if (container.attr('speed')) {
+                transformSpeed = container.attr('speed');
+            }
+        
+            // Make scrolling text copy for scrolling infinity
+            container.append(scrollingText.clone().addClass('scrolling-text-copy'));
+            container.find('.scrolling-text').css({'position': 'absolute', 'left': 0});
+            container.css('height', scrollingTextHeight);
+        
+            var getActiveScrollingText = function(direction) {
+                var firstScrollingText = container.find('.scrolling-text:nth-child(1)');
+                var secondScrollingText = container.find('.scrolling-text:nth-child(2)');
+        
+                var firstScrollingTextLeft = parseInt(container.find('.scrolling-text:nth-child(1)').css("left"), 10);
+                var secondScrollingTextLeft = parseInt(container.find('.scrolling-text:nth-child(2)').css("left"), 10);
+        
+                if (direction === 'left') {
+                    return firstScrollingTextLeft < secondScrollingTextLeft ? secondScrollingText : firstScrollingText;
+                } else if (direction === 'right') {
+                    return firstScrollingTextLeft > secondScrollingTextLeft ? secondScrollingText : firstScrollingText;
+                }
+            }
+        
+            $(window).on('wheel', function(e) {
+                var delta = e.originalEvent.deltaY;
+                
+                if (delta > 0) {
+                    // going down
+                    transformAmount += transformSpeed * transformDirection;
+                    container.find('.scrolling-text .scrolling-text-content').css('transform', 'skewX(10deg)');
+                }
+                else {
+                    transformAmount -= transformSpeed * transformDirection;
+                    container.find('.scrolling-text .scrolling-text-content').css('transform', 'skewX(-10deg)');
+                }
+                setTimeout(function(){
+                    container.find('.scrolling-text').css('transform', 'translate3d('+ transformAmount * -1 +'px, 0, 0)');
+                }, 10);
+                setTimeout(function() {
+                    container.find('.scrolling-text .scrolling-text-content').css('transform', 'skewX(0)');
+                }, 500)
+        
+                // Boundaries
+                if (transformAmount < leftBound) {
+                    var activeText = getActiveScrollingText('left');
+                    activeText.css({'left': Math.round(leftBound - scrollingTextWidth - startLetterIndent) + 'px'});
+                    leftBound = parseInt(activeText.css("left"), 10);
+                    rightBound = leftBound + scrollingTextWidth + scrollAmountBoundary + startLetterIndent;
+        
+                } else if (transformAmount > rightBound) {
+                    var activeText = getActiveScrollingText('right');
+                    activeText.css({'left': Math.round(rightBound + scrollingTextWidth - scrollAmountBoundary + startLetterIndent) + 'px'});
+                    rightBound += scrollingTextWidth + startLetterIndent;
+                    leftBound = rightBound - scrollingTextWidth - scrollAmountBoundary - startLetterIndent;
+                }
+            });
+        })
+    }
+});
+
+// Infinity slider
+var slider = document.getElementById('slider'),
+    sliderItems = document.getElementById('slides'),
+    prev = document.getElementById('prev'),
+    next = document.getElementById('next');
+
+function slide(wrapper, items, prev, next) {
+  var posX1 = 0,
+      posX2 = 0,
+      posInitial,
+      posFinal,
+      threshold = 100,
+      slides = items.getElementsByClassName('slide'),
+      slidesLength = slides.length,
+      slideSize = items.getElementsByClassName('slide')[0].offsetWidth,
+      firstSlide = slides[0],
+      lastSlide = slides[slidesLength - 1],
+      cloneFirst = firstSlide.cloneNode(true),
+      cloneLast = lastSlide.cloneNode(true),
+      index = 0,
+      allowShift = true;
+  
+  // Clone first and last slide
+  items.appendChild(cloneFirst);
+  items.insertBefore(cloneLast, firstSlide);
+  wrapper.classList.add('loaded');
+  
+  // Mouse events
+  items.onmousedown = dragStart;
+  
+  // Touch events
+  items.addEventListener('touchstart', dragStart);
+  items.addEventListener('touchend', dragEnd);
+  items.addEventListener('touchmove', dragAction);
+  
+  // Click events
+  prev.addEventListener('click', function () { shiftSlide(-1) });
+  next.addEventListener('click', function () { shiftSlide(1) });
+  
+  // Transition events
+  items.addEventListener('transitionend', checkIndex);
+  
+  function dragStart (e) {
+    e = e || window.event;
+    e.preventDefault();
+    posInitial = items.offsetLeft;
+    
+    if (e.type == 'touchstart') {
+      posX1 = e.touches[0].clientX;
+    } else {
+      posX1 = e.clientX;
+      document.onmouseup = dragEnd;
+      document.onmousemove = dragAction;
+    }
+  }
+
+  function dragAction (e) {
+    e = e || window.event;
+    
+    if (e.type == 'touchmove') {
+      posX2 = posX1 - e.touches[0].clientX;
+      posX1 = e.touches[0].clientX;
+    } else {
+      posX2 = posX1 - e.clientX;
+      posX1 = e.clientX;
+    }
+    items.style.left = (items.offsetLeft - posX2) + "px";
+  }
+  
+  function dragEnd (e) {
+    posFinal = items.offsetLeft;
+    if (posFinal - posInitial < -threshold) {
+      shiftSlide(1, 'drag');
+    } else if (posFinal - posInitial > threshold) {
+      shiftSlide(-1, 'drag');
+    } else {
+      items.style.left = (posInitial) + "px";
+    }
+
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+  
+  function shiftSlide(dir, action) {
+    items.classList.add('shifting');
+    
+    if (allowShift) {
+      if (!action) { posInitial = items.offsetLeft; }
+
+      if (dir == 1) {
+        items.style.left = (posInitial - slideSize) + "px";
+        index++;      
+      } else if (dir == -1) {
+        items.style.left = (posInitial + slideSize) + "px";
+        index--;      
+      }
+    };
+    
+    allowShift = false;
+  }
+    
+  function checkIndex (){
+    items.classList.remove('shifting');
+
+    if (index == -1) {
+      items.style.left = -(slidesLength * slideSize) + "px";
+      index = slidesLength - 1;
+    }
+
+    if (index == slidesLength) {
+      items.style.left = -(1 * slideSize) + "px";
+      index = 0;
+    }
+    
+    allowShift = true;
+  }
+}
+
+slide(slider, sliderItems, prev, next);
